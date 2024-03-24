6a44ddec9ede028b8920d2cdbffdb068e195479d
dylanjamaludin <jamaludindylanedu@gmail.com>
2024-03-24 10:02:49 -0400

    created new chat box widget, inital preprarations to connect model.

--- Interface2/interactive_map_dock.py
+++ Interface2/interactive_map_dock.py
@@ -46,7 +46,70 @@ class interactive_map_widget(QWidget):
 # class MapWindow(QWidget):
 #     def __init__(self, base_coords):
 #         self.base_coords = base_coords
-#         # Setting up the widgets and layout
+#         # Setting up the widgets a<html>
+# #           <head>
+# #
+# #             <title>Widget export</title>
+# #
+# #             <!-- Load RequireJS, used by the IPywidgets for dependency management -->
+# #             <script
+# #               src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"
+# #               integrity="sha256-Ae2Vz/4ePdIu6ZyI/5ZGsYnb+m0JlOmKPjt6XZ9JJkA="
+# #               crossorigin="anonymous">
+# #             </script>
+# #
+# #             <!-- Load IPywidgets bundle for embedding. -->
+# #             <script
+# #               data-jupyter-widgets-cdn="https://unpkg.com/"
+# #               data-jupyter-widgets-cdn-only
+# #               src="https://cdn.jsdelivr.net/npm/@jupyter-widgets/html-manager@*/dist/embed-amd.js"
+# #               crossorigin="anonymous">
+# #             </script>
+# #
+# #             <!-- The state of all the widget models on the page -->
+# #             <script type="application/vnd.jupyter.widget-state+json">
+# #               {manager_state}
+# #             </script>
+# #           </head>
+# #
+# #           <body>
+# #
+# #             <h1>Widget export</h1>
+# #
+# #             <div id="first-slider-widget">
+# #               <!-- This script tag will be replaced by the view's DOM tree -->
+# #               <script type="application/vnd.jupyter.widget-view+json">
+# #                 {widget_views[0]}
+# #               </script>
+# #             </div>
+# #
+# #             <hrule />
+# #
+# #             <div id="second-slider-widget">
+# #               <!-- This script tag will be replaced by the view's DOM tree -->
+# #               <script type="application/vnd.jupyter.widget-view+json">
+# #                 {widget_views[1]}
+# #               </script>
+# #             </div>
+# #
+# #           </body>
+# #         </html>
+# #         """
+# #
+# #         manager_state = json.dumps(data['manager_state'])
+# #         widget_views = [json.dumps(view) for view in data['view_specs']]
+# #         rendered_template = html_template.format(manager_state=manager_state, widget_views=widget_views)
+# #
+# #         # Set HTML
+# #         self.web.setHtml(rendered_template)
+# #
+# #         # Add webengine to layout and add layout to widget
+# #         self.layout.addWidget(self.web)
+# #         self.setLayout(self.layout)
+# #
+# #         self.show()
+# #
+# #nd layout
 #         super().__init__()
 #         self.layout = QVBoxLayout()
 #         self.title = QLabel("<b>This is my title</b>")
@@ -68,69 +131,6 @@ class interactive_map_widget(QWidget):
 #         data = embed_data(views=[s1, s2, self.map])
 #
 #         html_template = """
-#         <html>
-#           <head>
-#
-#             <title>Widget export</title>
-#
-#             <!-- Load RequireJS, used by the IPywidgets for dependency management -->
-#             <script
-#               src="https://cdnjs.cloudflare.com/ajax/libs/require.js/2.3.4/require.min.js"
-#               integrity="sha256-Ae2Vz/4ePdIu6ZyI/5ZGsYnb+m0JlOmKPjt6XZ9JJkA="
-#               crossorigin="anonymous">
-#             </script>
-#
-#             <!-- Load IPywidgets bundle for embedding. -->
-#             <script
-#               data-jupyter-widgets-cdn="https://unpkg.com/"
-#               data-jupyter-widgets-cdn-only
-#               src="https://cdn.jsdelivr.net/npm/@jupyter-widgets/html-manager@*/dist/embed-amd.js"
-#               crossorigin="anonymous">
-#             </script>
-#
-#             <!-- The state of all the widget models on the page -->
-#             <script type="application/vnd.jupyter.widget-state+json">
-#               {manager_state}
-#             </script>
-#           </head>
-#
-#           <body>
-#
-#             <h1>Widget export</h1>
-#
-#             <div id="first-slider-widget">
-#               <!-- This script tag will be replaced by the view's DOM tree -->
-#               <script type="application/vnd.jupyter.widget-view+json">
-#                 {widget_views[0]}
-#               </script>
-#             </div>
-#
-#             <hrule />
-#
-#             <div id="second-slider-widget">
-#               <!-- This script tag will be replaced by the view's DOM tree -->
-#               <script type="application/vnd.jupyter.widget-view+json">
-#                 {widget_views[1]}
-#               </script>
-#             </div>
-#
-#           </body>
-#         </html>
-#         """
-#
-#         manager_state = json.dumps(data['manager_state'])
-#         widget_views = [json.dumps(view) for view in data['view_specs']]
-#         rendered_template = html_template.format(manager_state=manager_state, widget_views=widget_views)
-#
-#         # Set HTML
-#         self.web.setHtml(rendered_template)
-#
-#         # Add webengine to layout and add layout to widget
-#         self.layout.addWidget(self.web)
-#         self.setLayout(self.layout)
-#
-#         self.show()
-#
 #
 # if __name__ == '__main__':
 #     app = QApplication(sys.argv)
