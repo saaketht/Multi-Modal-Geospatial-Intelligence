52fc78e6a1244fa7c9bc2d8066f3dedb77bfd78c
Dani <daniella.figueroa10@gmail.com>
2024-04-07 13:59:57 -0400

    added qframe for screenshot

--- Interface2/interactive_map_dock.py
+++ Interface2/interactive_map_dock.py
@@ -13,15 +13,50 @@ from ipywidgets.embed import embed_data
 import folium
 import io
 from pathlib import Path
-
+from datetime import datetime
 from file_explorer_dock import *
 
+class CaptureFrame(QFrame):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setWindowFlags(Qt.WindowType.FramelessWindowHint | Qt.WindowType.WindowStaysOnTopHint)
+        self.setStyleSheet("background-color: rgba(0, 0, 0, 0.5); border: 2px solid white;")
+        self.setWindowOpacity(0.5)
+        self.resize(300, 200)  
+        self.setMouseTracking(True)  
+        self.mouse_pressed = False
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setRenderHint(QPainter.RenderHint.Antialiasing)
+        painter.fillRect(self.rect(), Qt.BrushStyle.NoBrush)
+        painter.setPen(Qt.GlobalColor.white)
+        painter.drawRect(self.rect().adjusted(0, 0, -1, -1))
+    
+    def mousePressEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.mouse_pressed = True
+            self.mouse_press_pos = event.globalPosition().toPoint()
+
+    def mouseMoveEvent(self, event):
+        if self.mouse_pressed:
+            new_pos = event.globalPosition().toPoint() - self.mouse_press_pos
+            self.move(new_pos)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.mouse_pressed = False
 
 class interactive_map_widget(QWidget):
     screenshotTaken = pyqtSignal(str)
 
-    def __init__(self):
+    def __init__(self, app_data_path):
         super().__init__()
+
+        #ADDED BC FILE_PATH PARAMETER NOT WORKING IN TAKE SCREENSHOT
+        self.uploads_folder = os.path.join(app_data_path, "uploads")
+        if not os.path.exists(self.uploads_folder):
+                os.makedirs(self.uploads_folder)
 
         self.m = folium.Map(
             location=[28.598, -81.1974], tiles="openstreetmap",
@@ -40,6 +75,11 @@ class interactive_map_widget(QWidget):
         testlayout.addWidget(testbutton)
         testwidget.setLayout(testlayout)
 
+        #for qframe
+        self.capture_frame = CaptureFrame(self)
+        self.capture_frame.hide()
+        testbutton.clicked.connect(self.show_capture_frame)
+        testbutton2.clicked.connect(self.take_screenshot)
 
         self.tabs.setCornerWidget(testwidget, Qt.Corner.TopRightCorner)
         self.tabs.setUsesScrollButtons(False)
@@ -117,8 +157,34 @@ class interactive_map_widget(QWidget):
         # new_y_position = int(self.height() - button_height - 10)
         # self.analyzeButton.move(new_x_position, new_y_position)
 
+    #displays qframe on the map
+    def show_capture_frame(self):
+        if self.capture_frame.isVisible():
+            self.capture_frame.hide()
+        else:
+            self.capture_frame.show()
+            self.capture_frame.raise_()
+            self.center_capture_frame()
 
+    def take_screenshot(self):
+            pixmap = self.w.grab(self.capture_frame.geometry())
+            timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')  
+            screenshot_filename = f"screenshot_{timestamp}.png"  
+            file_path = os.path.join(self.uploads_folder, screenshot_filename)
+            if pixmap.save(file_path, "PNG"):
+                self.screenshotTaken.emit(file_path)
+            self.capture_frame.hide()
+    
+    def center_capture_frame(self):
+        map_widget_rect = self.w.geometry()
+        map_widget_center = map_widget_rect.center()
+        capture_frame_size = self.capture_frame.size()
+        capture_frame_x = map_widget_center.x() - capture_frame_size.width() // 2
+        capture_frame_y = map_widget_center.y() - capture_frame_size.height() // 2
+        self.capture_frame.move(capture_frame_x, capture_frame_y)
+        '''
     def take_screenshot(self, file_path):
         pixmap = self.w.grab()
         if pixmap.save(file_path, "PNG"):
             self.screenshotTaken.emit(file_path)
+    '''
