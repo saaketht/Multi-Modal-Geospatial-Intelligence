7e5b0adaa578da6866fa9156169974e6c6fea649
dylanjamaludin <jamaludindylanedu@gmail.com>
2024-04-07 21:05:41 -0400

    removed the setEnabled feature in send_message method, its a work in progress.

--- Interface2/chatbox_file.py
+++ Interface2/chatbox_file.py
@@ -311,10 +311,11 @@ class Chat(QWidget):
                 self.chat_scroll_area.verticalScrollBar().setValue(self.chat_scroll_area.verticalScrollBar().maximum())
                 self.update()
 
-                self.send_button.setEnabled(False)
+                # self.send_button.setEnabled(False)
                 model_runnable = ModelRunnable(message, self.current_image_path, self.messages)
                 model_runnable.signals.response_received.connect(self.handle_model_response)
                 QThreadPool.globalInstance().start(model_runnable)
+
 
                 """
                 model_output = send_and_receive(message, self.current_image_path, self.messages)
